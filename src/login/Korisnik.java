ab5b29ea69b943d1a080486472edaa14a5832be5
predragpmf <predrag.lazarevic@student.pmf.unibl.org>
2022-03-20 23:56:41 +0100

    Obrisani stari fajlovi

--- src/login/Korisnik.java
+++
@@ -1,8 +0,0 @@
-package login;
-
-public class Korisnik {
-	protected String korisnickoIme;
-	protected String email;
-	protected String hasiranaLozinka;
-	// TODO: Sadrzi sve zajednicko za Profesora i Ucenika.
-}
--- src/login/Ucenik.java
+++
@@ -1,5 +0,0 @@
-package login;
-
-public class Ucenik extends Korisnik {
-	// TODO: Na osnovu dobijenog korisnickog imena, povuci iz baze sve pristupne_podatke.
-}
